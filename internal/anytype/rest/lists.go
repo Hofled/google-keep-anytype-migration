aeba6a4e23993d82f78d44f4b1bf1a9ab460b946
Hofled <ofri.herold@gmail.com>
2026-04-28 11:01:54 +0200

    added support for add objects to list REST API

---
+++ internal/anytype/rest/lists.go
@@ -0,0 +1,45 @@
+package rest
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func formatAddObjectsToListEndpoint(spaceId, listId string) string {
+	return fmt.Sprintf("spaces/%s/lists/%s/objects", url.PathEscape(spaceId), url.PathEscape(listId))
+}
+
+type AddObjectsToListRequest struct {
+	Objects []string `json:"objects"`
+}
+
+func (c *Client) AddObjectsToList(ctx context.Context, spaceId, listId string, objectIds []string) error {
+	reqObj := AddObjectsToListRequest{objectIds}
+
+	buf := &bytes.Buffer{}
+	if err := json.NewEncoder(buf).Encode(&reqObj); err != nil {
+		return err
+	}
+
+	req, err := c.newRequest(ctx, http.MethodPost, formatAddObjectsToListEndpoint(spaceId, listId), buf)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return &InvalidResponseErr{resp}
+	}
+
+	return nil
+}
