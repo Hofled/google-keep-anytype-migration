5f12aebf8f81ce4e8f8bdaa3f6dcafea2bff9018
Hofled <ofri.herold@gmail.com>
2026-04-29 18:28:01 +0200

    added global app button styles

--- internal/tui/styles/styles.go
+++ internal/tui/styles/styles.go
@@ -5,4 +5,17 @@ import "charm.land/lipgloss/v2"
 var (
 	DisabledText = lipgloss.NewStyle().Strikethrough(true).Faint(true)
 	ErrText      = lipgloss.NewStyle().Foreground(lipgloss.Red)
+
+	buttonBackground         = lipgloss.Color("#ff58c4")
+	buttonDisabledBackground = lipgloss.Color("#b2b2b2")
+
+	baseButtonStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFF7DB")).Padding(0, 3)
+
+	ButtonStyle         = baseButtonStyle.Background(buttonBackground)
+	ButtonDisabledStyle = baseButtonStyle.Background(buttonDisabledBackground).Strikethrough(true)
 )
+
+func SelectedButton(buttonStyle lipgloss.Style) lipgloss.Style {
+	newBackground := lipgloss.Lighten(buttonStyle.GetBackground(), 0.1)
+	return buttonStyle.Background(newBackground).Underline(true)
+}
