ae3a44f654ed180c9c9b5654add3484ea058cfa2
Hofled <ofri.herold@gmail.com>
2026-04-30 22:21:31 +0200

    refactored list content formatting

--- internal/migrate/page.go
+++ internal/migrate/page.go
@@ -15,32 +15,31 @@ func GoogleNoteToCreatePageRequest(note googlekeep.Note) rest.CreateObjectReques
 		title = time.UnixMicro(int64(note.CreatedTimestampUsec)).Format(time.RFC822)
 	}
 
-	body := note.TextContent
+	var bodyBuilder strings.Builder
+
 	if len(note.ListContent) > 0 {
-		body = listContentToPageBody(note.ListContent)
+		bodyBuilder.WriteString(listContentToPageBody(note.ListContent))
+	} else if len(note.TextContent) > 0 {
+		bodyBuilder.WriteString(note.TextContent)
 	}
 
 	return rest.CreateObjectRequest{
 		TypeKey: "page",
 		Name:    title,
-		Body:    body,
+		Body:    bodyBuilder.String(),
 	}
 }
 
 func listContentToPageBody(listContent []googlekeep.ListContent) string {
 	var b strings.Builder
 
-	for i, item := range listContent {
+	for _, item := range listContent {
 		boxMarking := " "
 		if item.IsChecked {
 			boxMarking = "x"
 		}
 
-		b.WriteString(fmt.Sprintf("- [%s] %s", boxMarking, item.Text))
-
-		if i < len(listContent)-1 {
-			b.WriteString("\n")
-		}
+		fmt.Fprintf(&b, "- [%s] %s\n", boxMarking, item.Text)
 	}
 
 	return b.String()
